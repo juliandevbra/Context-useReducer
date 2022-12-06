89221ada27d6b9000f538b1c86d3cf8c58e8de8b
unknown <DH@DH-CARRERA-34.DH.digitalhouse.com>
2022-12-06 16:19:04 -0300

    Favs added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,25 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useGlobalStates } from '../Context/Context'
 
 const Home = () => {
 
     const {state, data, loading, setLoading} = useGlobalStates()
+    const [fav, setFav] = useState([])
 
     useEffect(() => {
         setTimeout(( ) => {
             setLoading(false)
         }, 2000)
     }, [state.api])
+    
+    useEffect(() => {
+        localStorage.setItem('favs', JSON.stringify(fav))
+    }, [fav])
 
+    const addFav = (imagen) => {
+        setFav([...fav, imagen])
+    }
+    
   return (
     <div className='card-section'>
         {console.log(data)}
@@ -19,12 +28,14 @@ const Home = () => {
         state.api === 'dog' ?
             data.message?.map((imagen, index) => <div key={index} className='card'>
                 <img src={imagen}  alt='' width={200} />
+                <button onClick={() => addFav(imagen)}>⭐</button>
             </div>
             )
             :
             data.map(imagen => (
                 <div className='card' >
                     <img src={imagen.url} alt="" width={200}  />
+                    <button onClick={() => addFav(imagen)}>⭐</button>
                 </div>
             ))
         }
